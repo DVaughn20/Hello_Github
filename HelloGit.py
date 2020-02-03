b77429dfd341dd3d8ce7c3106414e51f6666f71c
Devren Vaughn <dvaughn3323@gmail.com>
2020-02-03 08:40:04 -0600

    First python

---
+++ HelloGit.py
@@ -0,0 +1,17 @@
+# comment
+print("Hello World!")
+a = 6
+b = 4
+print(a+b)
+
+#how to make a list
+myList = [1, 2, 3]
+
+#how to access elements of a list
+print(myList[1])
+
+#how to create a function
+def sumFunction(a,b):
+	return a + b
+
+print(sumFunction(2, 20))
