8595de9f5de68285da007d0d331a1d004f4b2fcd
Devren Vaughn <dvaughn3323@gmail.com>
2020-02-06 09:35:12 -0600

    While loop Added

--- HelloGit.py
+++ HelloGit.py
@@ -10,6 +10,12 @@ myList = [1, 2, 3]
 #how to access elements of a list
 print(myList[1])
 
+d = 2
+while d < 20:
+  print(d)
+  d += 2
+
+
 #how to create a function
 def sumFunction(a,b):
 	return a + b
