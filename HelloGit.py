782b50d98b8baf530163a306a599ad595901cd98
Devren Vaughn <dvaughn3323@gmail.com>
2020-02-10 08:28:39 -0600

    Added input and output

--- HelloGit.py
+++ HelloGit.py
@@ -24,3 +24,25 @@ print(sumFunction(2, 20))
 
 print("hello world")
 
+
+#dictionary
+#lookup{}
+
+
+
+#three datatypes at input
+
+#float
+someFloat = float(input("Enter a float: "))
+print("You entered float: " + str(someFloat))
+print(f"you entered float: {someFloat}" )
+#string
+phrase = input ("Enter a String: ")
+print("you said " + phrase)
+print(f"you said {phrase}")
+#int
+someInt = float(input("Enter an int: "))
+print("You entered int: " + str(someInt))
+print(f"you entered int: {someInt}" )
+
+print(f"Do Python inline, like this: {someFloat} * {someInt} = {someFloat} * {someInt}")
